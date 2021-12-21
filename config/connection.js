22cee4245e1526caa433a438dd9d364f6c8eac07
LohasOT <oliverhoang93@gmail.com>
2021-12-21 00:06:53 -0800

    working connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const Sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+const sequelize = new Sequelize('mysql://root:Lohas199312!@localhost:3306/ecommerce_db')
 
 module.exports = sequelize
